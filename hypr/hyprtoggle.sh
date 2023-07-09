73f340d65a23593508af752860d7006a56810656
darkstack <1835601+darkstack@users.noreply.github.com>
2023-07-09 23:19:58 +0200

    Toggle config layout / master_is_new

---
+++ hypr/hyprtoggle.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+case $4 in 
+    int)
+        NRV=2
+        ;;
+    float)
+        NRV=3
+        ;;
+    str)
+        NRV=4
+        ;;
+    *)
+        NRV=2
+esac
+VALUE=$(hyprctl getoption $1 | awk -v nrv=$NRV 'NR==nrv {print $2}' | sed "s/\"//g")
+
+if [ "$VALUE" == $2 ]; then
+    hyprctl keyword $1 $3
+else
+    hyprctl keyword $1 $2
+fi
+echo $VALUE
